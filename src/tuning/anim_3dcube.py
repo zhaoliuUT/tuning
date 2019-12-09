5a0cf9fe6ef43913575ae8213483d3d004e0f75f
Zhao <zliu@math.utexas.edu>
2019-12-09 17:49:31 -0600

    add static figure of 3d cube plots

--- src/tuning/anim_3dcube.py
+++ src/tuning/anim_3dcube.py
@@ -32,6 +32,209 @@ class Arrow3D(FancyArrowPatch):
         self.set_positions((xs[0], ys[0]), (xs[1], ys[1]))
         FancyArrowPatch.draw(self, renderer)
 
+# static figure of 3d cube plot        
+def cube3dplots(X, Y, Z, weight = None, info = None, 
+                radius = 1, weight_tol = 1e-3, cmap_name = 'nipy_spectral', shuffle_colors = False,
+                INCLUDE_FUN = True, INCLUDE_WEIGHT = True, INCLUDE_WEIGHT_BAR = True,
+                FILE_NAME = "", ADD_TIME = True):
+    
+    '''When numNeuro = 3, plot points in a cube.'''
+    ''' the 'radius' argument acts the same way as maxium FP.'''
+        
+    if len(X)!=len(Y) or len(Y)!=len(Z):
+        raise Exception('Wrong dimension of inputs!')
+
+    num_pts = len(X) # number of points
+    
+    if weight is None:
+        PLOT_WEIGHTS = False
+    elif len(weight)!= num_pts:
+        raise Exception('Wrong dimension of inputs: weight')
+    elif np.fabs(np.sum(weight) - 1)>1e-5:
+        raise Exception('Wrong input of weight: sum error!')
+    else:
+        PLOT_WEIGHTS = True
+        
+    X_list = [X]
+    Y_list = [Y]
+    Z_list = [Z]
+    if weight is not None:
+        weights_list = [weight]
+    else:
+        weights_list = None
+    if info is not None:
+        info_list = [info]
+    else:
+        info_list = None
+    
+    # figure setup
+    if INCLUDE_FUN or INCLUDE_WEIGHT or INCLUDE_WEIGHT_BAR:
+        fig = plt.figure(figsize = (12, 6 + 1.0*INCLUDE_WEIGHT_BAR))
+        gs0 = gridspec.GridSpec(1, 2, width_ratios=[2, 1])
+        n_cols = 3*INCLUDE_FUN + INCLUDE_WEIGHT + INCLUDE_WEIGHT_BAR
+        ax_w_bar_idx = 3*INCLUDE_FUN
+        ax_w_idx = 3*INCLUDE_FUN + INCLUDE_WEIGHT_BAR
+
+        height_ratios = np.ones(n_cols)
+        if INCLUDE_WEIGHT_BAR:
+            height_ratios[ax_w_bar_idx] = 0.5
+        gs01 = gridspec.GridSpecFromSubplotSpec(n_cols,1,subplot_spec=gs0[1], height_ratios=height_ratios)
+
+
+        ax_cube = fig.add_subplot(gs0[0], projection='3d')
+        ax_cube.set_aspect("equal")
+        if INCLUDE_FUN:
+            ax_f1 = fig.add_subplot(gs01[0])
+            ax_f2 = fig.add_subplot(gs01[1])
+            ax_f3 = fig.add_subplot(gs01[2])
+        if INCLUDE_WEIGHT_BAR:
+            ax_w_bar = fig.add_subplot(gs01[ax_w_bar_idx])
+            ax_w_bar.set_xlim([0, 1])
+            ax_w_bar.set_ylim([-0.1, 0.1])
+            ax_w_bar.set_aspect(0.2)
+            ax_w_bar.set_yticks([])
+        if INCLUDE_WEIGHT:
+            ax_w = fig.add_subplot(gs01[ax_w_idx])
+            weight_max = np.max(np.array(weights_list))
+            ax_w.set_ylim(0, weight_max + 0.05)
+    else:
+        fig = plt.figure(figsize = (8,8))
+        ax_cube = fig.gca(projection='3d')
+        ax_cube.set_aspect("equal")
+        
+
+    # draw cube
+    r = [0, radius]
+    for s, e in combinations(np.array(list(product(r, r, r))), 2):
+        if np.sum(np.abs(s-e)) == r[1]-r[0]:
+            ax_cube.plot3D(*zip(s, e), color="k", alpha = 0.5, lw = 1.5)#linestyle='--'
+
+    r2 = [0, 1.4*radius]
+    for s in np.array(list(product(r2, r2, r2))):
+        if np.sum(s> 0) == 1:
+            a = Arrow3D(*zip([0,0,0], s), mutation_scale=20,
+                lw=1, arrowstyle="-|>", color="k")
+            ax_cube.add_artist(a)
+    ax_cube._axis3don = False
+    ax_cube.grid(True)
+    ax_cube.set_xlim([0,1.5*radius])
+    ax_cube.set_ylim([0,1.5*radius])
+    ax_cube.set_zlim([0,1.5*radius])
+    endpts = [[1.45*radius,-0.1*radius,0],[0.1*radius,1.45*radius,0],[0,0,1.45*radius]]
+    
+    
+    for k in range(len(endpts)):
+        ax_cube.text(endpts[k][0],endpts[k][1],endpts[k][2],r'$f_%d$'%(k+1),color = 'k',fontsize = 16)
+
+
+    ax_cube.view_init(azim = 30)
+
+    # color_arr = np.arange(num_pts)
+    cmap = plt.cm.get_cmap(cmap_name, num_pts) # other colormap names: cubehelix,viridis,...
+    color_arr = np.array([cmap(i) for i in range(num_pts)])
+    if shuffle_colors:
+        np.random.shuffle(color_arr)
+    
+    scat = ax_cube.scatter(X_list[0], Y_list[0], Z_list[0],  c = color_arr,s= 100, edgecolor='k', alpha=1)
+    if info_list is not None:
+        x0, y0, _ = proj3d.proj_transform(0,-0.3*radius, -1.5*radius,ax_cube.get_proj())
+        info_txt = ax_cube.text2D(x0,y0,"", fontsize = 15)
+    
+    
+    txt_list = [ax_cube.text2D(0,0,"",fontsize = 14, color = 'k') for _ in range(num_pts)]  # 'steelblue'
+    if INCLUDE_FUN:
+        line1, = ax_f1.plot([], [], color = 'crimson', lw = 2)
+        line2, = ax_f2.plot([], [], color = 'seagreen', lw = 2)
+        line3, = ax_f3.plot([], [], color = 'steelblue', lw = 2)
+        ax_f_list = [ax_f1, ax_f2, ax_f3]
+        for ax_f in ax_f_list:
+            ax_f.set_xlim([0,1])
+            ax_f.set_ylim([0,radius*1.01])
+            ax_f.set_yticks([0,radius])
+            ax_f.spines['top'].set_visible(False)
+            ax_f.spines['right'].set_visible(False)
+            ax_f.yaxis.set_ticks_position('left')
+            ax_f.xaxis.set_ticks_position('bottom')
+            #ax_f.arrow(0,0,1,0,fc='k', ec='k', lw =0.1,head_width=0.05, head_length=0.02,overhang = 0.1,\
+            #           length_includes_head= False, clip_on = False)
+            #ax_f.arrow(0,0,0,radius,fc='k', ec='k', lw =0.1,head_width=0.01*radius, head_length=0.1*radius,\
+            #           overhang = 0.1*radius,\
+            #           length_includes_head= False, clip_on = False)
+    if INCLUDE_WEIGHT:
+        barcollection = ax_w.bar(np.arange(num_pts),weights_list[0], color = color_arr)
+    if INCLUDE_WEIGHT_BAR:
+        rect_list = []
+        for j in range(num_pts):
+            rect = plt.Rectangle((np.sum(weights_list[0][:j]), -0.1), weights_list[0][j], 0.2,
+                                 facecolor=color_arr[j])
+            ax_w_bar.add_artist(rect)
+            rect_list.append(rect)
+    #     plt.tight_layout()#pad = 1.5
+    #     gs0.tight_layout(fig)
+    #     def init():     
+    #         return scat,
+
+    # animation function.  This is called sequentially
+    def plot_figure(i):
+
+        scat._offsets3d = juggle_axes(X_list[i], Y_list[i], Z_list[i],'z') #'z',color=color_set[0],s= 100
+        sizes = np.ones(num_pts)*100
+        if PLOT_WEIGHTS:
+            sizes[weights_list[i] < weight_tol] = 0
+        scat.set_sizes(sizes)
+        if PLOT_WEIGHTS:
+            for txt, new_x, new_y, new_z, weight in zip(txt_list, X_list[i], Y_list[i], Z_list[i],weights_list[i]):
+                # animating Text in 3D proved to be tricky. Tip of the hat to @ImportanceOfBeingErnest
+                # for this answer https://stackoverflow.com/a/51579878/1356000
+                if weight > weight_tol:
+                    x_, y_, _ = proj3d.proj_transform(new_x+0.1*radius, new_y+0.1*radius, new_z+0.1*radius, \
+                                                      ax_cube.get_proj())
+                    txt.set_position((x_,y_))
+                    txt.set_text('%.2f'%weight)
+                else:
+                    txt.set_text("")
+        if INCLUDE_FUN and (weights_list is not None):
+            x1, y1 = pc_fun_weights(X_list[i],weights_list[i])
+            x2, y2 = pc_fun_weights(Y_list[i],weights_list[i])
+            x3, y3 = pc_fun_weights(Z_list[i],weights_list[i])
+            line1.set_data(x1, y1)
+            line2.set_data(x2, y2)
+            line3.set_data(x3, y3)
+        if INCLUDE_FUN and (weights_list is None):
+            line1.set_data(1.0*np.arange(num_pts)/num_pts, X_list[i])
+            line2.set_data(1.0*np.arange(num_pts)/num_pts, Y_list[i])
+            line3.set_data(1.0*np.arange(num_pts)/num_pts, Z_list[i])
+
+        if INCLUDE_WEIGHT:
+            for j, b in enumerate(barcollection):
+                b.set_height(weights_list[i][j])
+        if INCLUDE_WEIGHT_BAR:
+            for j in range(num_pts):
+                rect_list[j].set_x(np.sum(weights_list[i][:j]))
+                rect_list[j].set_width(weights_list[i][j])
+
+        if info_list is not None:
+            info_txt.set_text("MI = %.4f"%info_list[i])
+        return scat
+    
+    _ = plot_figure(0)
+    # call the animator.  blit=True means only re-draw the parts that have changed.
+    #anim = animation.FuncAnimation(fig, animate,
+    #                               frames = num_frames, interval=interval)#, blit=True      
+    if ADD_TIME: 
+        timestr = time.strftime("%m%d-%H%M%S")
+    else:
+        timestr = ""
+    filename =  FILE_NAME + timestr + ".png" 
+    directory = os.path.dirname(filename)
+    if directory != "":
+        try:
+            os.stat(directory)
+        except:
+            os.makedirs(directory)  
+    plt.savefig(filename)
+
+# animation
 def anim3dplots(X_list, Y_list, Z_list, weights_list = None, info_list = None, 
                 radius = 1, weight_tol = 1e-3, cmap_name = 'nipy_spectral', shuffle_colors = False,
                 INCLUDE_FUN = True, INCLUDE_WEIGHT = True, INCLUDE_WEIGHT_BAR = True,
