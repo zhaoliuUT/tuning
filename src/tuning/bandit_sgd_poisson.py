051da48f4b0d8cbd241b2e0ac3fc0d4eb02c956a
Zhao <zliu@math.utexas.edu>
2020-02-12 16:17:53 -0600

    modify poisson_bandit_iteration: same algo as gaussian

--- src/tuning/bandit_sgd_poisson.py
+++ src/tuning/bandit_sgd_poisson.py
@@ -52,55 +52,83 @@ def poisson_DKL(bin_index, poisson_samples, input_set, input_prob_vec):
 
 # -----------Bandit Algorithm for Arimoto iterations-----------
 
-def poisson_bandit_iteration(input_set, discount_factor = 0.9, epsilon=0, max_iter = 1000):
+def poisson_bandit_iteration(input_set,
+                             initial_prob_vec = None, max_iter = 1000, batch_size = 1,
+                             dkl_discount_factor = "decrease", epsilon=0,
+                             update_rule = "additive",#"direct", "multiply"
+                             initial_learning_rate = 0.01, learning_rate_decrease_rate = 0):
     # 'temporal difference scheme'
-    # discount_factor = 0 is the same as using argmax(rewards) (ignoring path history)
+    # dkl_discount_factor = "decrease" or [0,1) e.g. 0.9
+    # dkl_discount_factor = 0 is the same as using argmax(rewards) ignoring path history
     # epsilon probability on selecting other choices
+
+    if not (dkl_discount_factor == "decrease" or (dkl_discount_factor >=0 and dkl_discount_factor <=1)):
+        raise Exception("dkl_discount_factor must be in [0,1] or 'decrease'!")
+    if not (update_rule in ["additive", "direct", "multiply"]):
+        raise Exception("update_rule must be in ['additive', 'direct','multiply']!")
+
     if input_set.ndim == 1:
         input_set = input_set.reshape((1,-1))
     numNeuro, numBin = input_set.shape
     #num_inputs = numBin
-    counts = np.ones(numBin, dtype = np.int) # initialize with 1   
-    curr_prob_vec = 1.0*counts/np.sum(counts) # initalize with equal weights
-    #curr_prob_vec = input_prob_vec.copy()
+    if initial_prob_vec is None:
+        prob_vec = 1.0*np.ones(numBin)/numBin # initalize with equal weights
+    else:
+        prob_vec = initial_prob_vec.copy()
+
     rewards = np.zeros(numBin)
     DKL_estimates = np.zeros(numBin)
     
     num_iter = 0
     rewards_list = []
     DKL_estimates_list = []
+    prob_vec_list = [prob_vec.copy()]
     
     # generate poisson samples in advance
-    poisson_samples = np.zeros((max_iter, numNeuro, numBin), dtype=np.int)
+    poisson_samples = np.zeros((max_iter*batch_size, numNeuro, numBin), dtype=np.int)
     for p in range(numNeuro):
         for j in range(numBin):
-            poisson_samples[:, p,j] = np.random.poisson(lam = input_set[p,j], size = max_iter)
-    
+            poisson_samples[:, p,j] = np.random.poisson(lam = input_set[p,j], size = max_iter*batch_size)
+
     while(num_iter < max_iter):
-        for i in range(numBin):
-            # compute rewards by sampling
-            output_sample = poisson_samples[num_iter, :, i]
-            rewards[i] = poisson_log_ratio(i, output_sample, input_set, curr_prob_vec)
-        rewards_list.append(rewards.copy())
+        rewards *= 0
+        for k in range(batch_size):
+            for i in range(numBin):
+                # compute rewards by sampling
+                output_sample = poisson_samples[num_iter*batch_size+k, :, i]
+                rewards[i] += poisson_log_ratio(i, output_sample, input_set, prob_vec)
+        rewards /= batch_size
         
         # update the DKL_estimates with discounting factor       
-        if discount_factor == "change":
-            DKL_estimates = 1.0/(num_iter + 1)*rewards + 1.0*num_iter/(num_iter + 1)*DKL_estimates                
+        if dkl_discount_factor == "decrease":
+            DKL_estimates = 1.0/(num_iter+1)*rewards + 1.0*num_iter/(num_iter+1)*DKL_estimates
         else:
-            DKL_estimates = (1-discount_factor)*rewards + discount_factor*DKL_estimates
+            DKL_estimates = (1-dkl_discount_factor)*rewards + dkl_discount_factor*DKL_estimates
+
+        alpha = initial_learning_rate/(learning_rate_decrease_rate*num_iter + 1)
+
+        if update_rule == "additive":
+            u = np.random.uniform()
+            if u < epsilon:
+                optimal_index = np.random.choice(numBin)
+            else:
+                optimal_index = np.argmax(DKL_estimates)
+            prob_vec *= 1.0 - alpha
+            prob_vec[optimal_index] += alpha
+        elif update_rule == "multiply":
+            prob_vec *= np.exp(rewards)**alpha
+            prob_vec /= np.sum(prob_vec)
+        elif update_rule == "direct":
+            prob_vec *= np.exp(DKL_estimates)
+            prob_vec /= np.sum(prob_vec)
             
-        u = np.random.uniform()
-        if u < epsilon:
-            optimal_index = np.random.choice(numBin)
-        else:
-            optimal_index = np.argmax(DKL_estimates)
-            
+        rewards_list.append(rewards.copy())
         DKL_estimates_list.append(DKL_estimates.copy())
+        prob_vec_list.append(prob_vec.copy())
         
-        counts[optimal_index] += 1
-        curr_prob_vec = 1.0*counts/np.sum(counts)
         num_iter += 1
-    return curr_prob_vec, rewards_list, DKL_estimates_list
+
+    return prob_vec, rewards_list, DKL_estimates_list, prob_vec_list
 
 # -----------SGD Algorithm-----------
 
