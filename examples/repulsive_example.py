7f4fb6e914bd851bcf28a665ecaca6010e2fdba2
Zhao <zliu@math.utexas.edu>
2020-05-23 17:30:09 -0500

    add repulsive model codes and example

---
+++ examples/repulsive_example.py
@@ -0,0 +1,282 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import colors
+import time
+import os
+import scipy
+from scipy import optimize
+from sklearn.cluster import KMeans
+from scipy import special
+
+import tuning
+from tuning.cyMINoncyclic import mc_mean_grad_gaussian, mc_mean_grad_gaussian_inhomo # gaussian models
+from tuning.cyMINoncyclic import mc_coeff_arimoto_gaussian, mc_coeff_arimoto_gaussian_inhomo # gaussian models
+from tuning.repulsive_model import tuning_update_inhomo_periodic, conditional_probability_matrix_vonmises
+from tuning.repulsive_model import evaluate_elastic_term_periodic, mutual_distance
+from tuning.functions_for_analysis import gen_binary_hierachical_curves
+from tuning.anim_3dcube import pc_fun_weights, plot_funcs_in_figure
+# for 3d illustration:
+# from tuning.anim_3dcube import create_figure_canvas, set_data_in_figure, get_color_array, gen_mixed_plots, gen_mixed_anim
+
+# ----------initialization---------  
+numNeuro = 5
+tc = gen_binary_hierachical_curves(numNeuro, fp = 0.8, fm = -0.8)
+
+# fig = plt.figure()
+# _ = plot_funcs_in_figure(fig, tc, np.ones(tc.shape[1]), nrow=numNeuro, ncol=1, fp = 1, fm = -1)
+
+numBin = 1024 #32*32
+tuning = np.zeros((numNeuro, numBin))
+num_pts = numBin/tc.shape[1]
+vec = np.linspace(0, 1, num_pts + 1)[:num_pts]
+for k in range(tc.shape[1]):
+    tuning[:, k*num_pts:(k+1)*num_pts] = tc[:, k][:, None] + vec*(tc[:,(k+1)%tc.shape[1]] - tc[:, k])[:, None]
+tuning[tuning < -1] = -1
+tuning[tuning > 1] = 1
+weight = 1.0*np.ones(numBin)/numBin
+    
+# fig = plt.figure()
+# _ = plot_funcs_in_figure(fig, tuning, weight, nrow=numNeuro, ncol=1, fp = 1, fm = -1)
+# from mpl_toolkits.mplot3d import Axes3D
+# if numNeuro ==2 :
+#     plt.figure()
+#     plt.scatter(tuning[0,:], tuning[1,:])
+#     plt.xlim([-1, 1])
+#     plt.ylim([-1, 1])
+# elif numNeuro == 3:
+#     fig = plt.figure()
+#     ax = fig.add_subplot(111, projection='3d')
+#     ax.scatter(tuning[0,:], tuning[1,:], tuning[2,:])
+
+#     ax.set_xlabel('x0')
+#     ax.set_ylabel('x1')
+#     ax.set_zlabel('x2')
+#     ax.set_xlim([-1, 1])
+#     ax.set_ylim([-1, 1])
+#     ax.set_zlim([-1, 1])
+
+#     plt.show()
+
+# ----------optimization function, derivative, bounds, constraint---------  
+# added self energy term
+def opt_fun(w, A, gamma):
+    return np.dot(w, (np.dot(A, w))) + gamma*np.sum(w**2)
+            
+def grad_fun(w, A, gamma):
+    return np.dot(A + np.transpose(A), w) + 2*gamma*w
+
+weight_bounds = []
+for j in range(len(weight)): # number of bins
+    weight_bounds += [(0, 1)]
+weight_bounds = tuple(weight_bounds)
+
+my_cons = [{'type':'eq', 'fun': lambda w: (np.sum(w) - 1)}]
+
+# ----------parameters of the Von-mises model---------
+kappa = 5
+kappa_mat = np.zeros_like(tuning)
+kappa_mat[:, :] = kappa
+# print(kappa_mat)
+alpha = 5
+gamma = 0
+
+# ---------initialization of results-saving-------
+tuning_list = [tuning.copy()]
+weight_list = [weight.copy()]
+
+opt_list = []
+info_list = []
+EVALUATE_OPT = True
+EVALUATE_EL = True
+EVALUATE_MI = False
+eta_list = [None]
+beta_list = [None]
+Lambda_list = [None]
+elastic_term_list = [None]
+elastic_value_list = [None]
+gamma_list = [gamma]
+
+tuning = tuning_list[0].copy()
+
+if EVALUATE_OPT:
+    P = conditional_probability_matrix_vonmises(tuning, kappa_mat, alpha = alpha)
+    opt_list += [opt_fun(weight, P, gamma)]
+    print(opt_list)
+#--------start iterations-----------
+ncol = 1
+nrow = numNeuro
+T = 100
+
+eta = 20
+beta = 0#5e-4
+
+elastic_term = 'exp'
+Lambda = 2
+
+ALTER_WEIGHT = T+1
+
+EVALUATE_OPT = True
+EVALUATE_EL = True
+EVALUATE_MI = False
+PLOT = True
+ALTER_PLOT = 10
+
+fp = 1; fm = -1
+
+if PLOT:
+    %matplotlib notebook
+
+    fig = plt.figure(figsize = (ncol*6, nrow*2))
+    ax_list = []
+    for i in range(numNeuro):
+        ax = fig.add_subplot(nrow,ncol,i+1)
+        ax_list.append(ax)
+
+    for i in range(numNeuro):
+        xx, yy = pc_fun_weights(tuning[i,:], weight)
+        ax_list[i].plot(xx, yy)
+        ax_list[i].set_ylim([fm, fp])
+    #     ax_list[i].scatter(np.arange(numBin), tuning[i, :])
+
+    fig.canvas.draw()
+
+        
+for num_iter in range(1, T+1):
+    
+    tuning_new = tuning_update_inhomo_periodic(
+        tuning, weight, kappa_mat, alpha = alpha,
+        eta = eta, beta = beta, Lambda = Lambda, elastic_term = elastic_term,
+    )
+    
+    tuning = tuning_new.copy()
+
+    
+    if num_iter % ALTER_WEIGHT ==0:        
+        D = conditional_probability_matrix_vonmises(tuning, kappa_mat, alpha=alpha)
+        disp = False
+        ftol = 1e-6
+        res = optimize.minimize(opt_fun, weight, args = (D,gamma), \
+                                method='SLSQP', jac = grad_fun, \
+                                bounds = weight_bounds, constraints = my_cons, \
+                                options = {'maxiter':WEIGHT_ITER_NUM, 'ftol': ftol, 'disp': disp})
+
+        weight_new = res['x'].copy()
+        print(res['fun'], opt_fun(weight, D, gamma))
+        #print(weight_new)
+        weight = weight_new.copy()
+        
+    tuning_list += [tuning.copy()]
+    weight_list += [weight.copy()]
+    
+    eta_list += [eta]
+    beta_list += [beta]
+    Lambda_list += [elastic_term]
+    elastic_term_list += [Lambda]
+    gamma_list += [gamma]
+    
+    if EVALUATE_OPT:
+        D = conditional_probability_matrix_vonmises(tuning, kappa_mat, alpha=alpha)   
+        opt_list += [opt_fun(weight, D, gamma)]     
+    if EVALUATE_EL:
+        elastic_value_list += [
+            evaluate_elastic_term_periodic(tuning, weight, alpha = alpha, 
+                                           beta = beta, Lambda=Lambda, 
+                                           elastic_term = elastic_term,
+                                           elastic_term_periodic = True,
+                                          )]
+
+#     if EVALUATE_MI:
+#         grad_tc = np.zeros_like(tuning)       
+#         info_tc = mc_mean_grad_gaussian_inhomo(
+#             grad_tc, tuning, weight,
+#             inv_cov_matrix, conv, tau, int(1e4), my_num_threads = NUM_THREADS)
+#         info_list += [info_tc/np.log(2)]
+
+    if PLOT and num_iter%ALTER_PLOT==0:
+        for i in range(numNeuro):
+            ax_list[i].clear()
+            xx, yy = pc_fun_weights(tuning[i,:], weight)
+            ax_list[i].plot(xx, yy)
+            ax_list[i].set_ylim([fm, fp])
+#         time.sleep(0.1)
+        fig.canvas.draw()
+    if num_iter%10 == 0:
+        print('num_iter=%d, opt = %.2f'%(num_iter, opt_list[-1]))
+        
+#-------showing and saving results----------
+# plt.figure()
+# plt.plot(opt_list)
+
+# plt.figure()
+# plt.plot(elastic_value_list)
+
+# #--comparing histograms of mutual distances----
+# tc = tuning_list[0]
+# num_nearst_neighbours = 50
+# dS = mutual_distance(tc)
+# sorted_dS = np.sort(dS, axis = 1)
+# plt.figure()
+# _ = plt.hist(sorted_dS[:, 1:num_nearst_neighbours + 1].reshape(-1), bins = 100)
+
+# tc = tuning_list[-1]
+# dS = mutual_distance(tc)
+# num_nearst_neighbours = 50
+# sorted_dS = np.sort(dS, axis = 1)
+# plt.figure()
+# _ = plt.hist(sorted_dS[:, 1:num_nearst_neighbours + 1].reshape(-1), bins = 100)
+
+
+FILE_NAME = "dim=%d_kappa=%.2f_alpha=%.2f_beta=%.1e_"%(
+    numNeuro, kappa_mat[0,0],alpha, beta) + time.strftime("%m%d-%H%M%S")
+
+res_dict = {}
+res_dict['kappa'] = kappa_mat
+res_dict['alpha'] = alpha
+
+res_dict['eta'] = eta_list
+res_dict['beta'] = beta_list
+res_dict['gamma'] = gamma_list
+res_dict['Lambda'] = Lambda_list
+res_dict['elastic_term'] = elastic_term_list
+
+res_dict['tuning'] = tuning_list
+res_dict['weight'] = weight_list
+
+res_dict['opt'] = opt_list
+res_dict['info'] = info_list
+res_dict['elastic'] = elastic_value_list
+
+print(FILE_NAME)
+np.save(FILE_NAME, res_dict)
+
+# fig = plt.figure()
+# ax_list = plot_funcs_in_figure(fig, tuning, weight, ncol = 1, nrow = numNeuro, fp = 1, fm = -1)
+# ax_list[0].set_title(r'$\kappa = %.1f, \alpha = %.1f, \beta = %.1e$, F(x,w) = %.4f'%(kappa_mat[0, 0], alpha, beta, opt_list[-1]))
+# plt.savefig('./figures/' + FILE_NAME+'.jpg')
+
+# from mpl_toolkits.mplot3d import Axes3D
+# if numNeuro ==2 :
+#     plt.figure()
+#     plt.scatter(tuning[0,:], tuning[1,:])
+#     if beta != 0:
+#         plt.plot(tuning[0, :], tuning[1, :], '--r', linewidth = 0.5)
+#     plt.xlim([-1, 1])
+#     plt.ylim([-1, 1])
+#     plt.savefig('./figures/' + FILE_NAME+'_points.jpg')
+# elif numNeuro == 3:
+#     fig = plt.figure()
+#     ax = fig.add_subplot(111, projection='3d')
+#     ax.scatter(tuning[0,:], tuning[1,:], tuning[2,:])
+#     if beta != 0:
+#         ax.plot(tuning[0, :], tuning[1, :], tuning[2, :], '--r', linewidth = 0.5)
+
+#     ax.set_xlabel('x0')
+#     ax.set_ylabel('x1')
+#     ax.set_zlabel('x2')
+#     ax.set_xlim([-1, 1])
+#     ax.set_ylim([-1, 1])
+#     ax.set_zlim([-1, 1])
+
+#     ax.set_title(r'$\kappa = %.1f, \alpha = %.1f, \beta = %.1f$, F(x,w) = %.4f'%(kappa_mat[0, 0], alpha, beta, opt_list[-1]))
+#     plt.show()
+#     plt.savefig('./figures/' + FILE_NAME+'_points.jpg')
---
+++ src/tuning/repulsive_model.py
@@ -0,0 +1,275 @@
+import numpy as np
+import time
+import os
+from scipy import special
+
+def tuning_update_inhomo(tuning, weight, sigma_vec, alpha = 2, eta = 1, beta = 0, Lambda = 1,
+                         elastic_term = 'sum',# other options: 'exp', 'expweight', 'rand'
+                         elastic_term_periodic = True, # use closed curve
+                         upper_bound = 1, lower_bound = 0):
+    '''Gaussian Model, no periodic boundary condition
+    '''
+    # tuning.shape = (numNeuro, numBin)
+    # weight: vector, sum =1, length = numBin
+    # sigma_vec: vector, gaussian std, length = numBin
+    # Lambda: number, std for the laplacian term
+    # eta: number, coefficient of the repulsive term
+    # alpha: number, exponent of \|x_k - x_l\| in the probability distribution
+    # beta: number, coefficient of the laplacian term
+    nNeuro, nBin = tuning.shape
+    dS = np.zeros((nBin, nBin))
+    for i in range(nNeuro):
+        dX = tuning[i,:][None, :] - tuning[i,:][:, None] # dX[k, l]=tuning[i, l]-tuning[i, k]
+        dS += dX**2
+    dS = np.sqrt(dS)
+    # dS[k,l]: euclidean distance between the k-th point and the l-th point    
+
+    
+    # negative derivatives
+    u = np.zeros((nNeuro, nBin))
+    F = (dS**(alpha-2))*np.exp(-dS**alpha/(2*sigma_vec[:,None]**alpha))/sigma_vec[:,None]**(alpha+1)
+    F2 = (dS**(alpha-2))*np.exp(-dS**alpha/(2*sigma_vec[None,:]**alpha))/sigma_vec[None, :]**(alpha+1)
+    for i in range(nNeuro):
+        xi = tuning[i,:]
+        dX = xi[None, :] - xi[:, None]
+        u[i,:] = np.sum(weight[:,None]*dX*F,axis = 0)
+        
+        # inside the sum:
+        # [k,l] = weight[k]*dX[k,l]*(dS[k,l]**(alpha-2))
+        # *np.exp(-dS[k,l]**alpha/(2*sigma_vec[k]**alpha))/sigma_vec[k]**(alpha+1)
+        # = weight[k]*(x[l]_i-x[k]_i)*(dS[k,l]**(alpha-2))
+        # *np.exp(-dS[k,l]**alpha/(2*sigma_vec[k]**alpha))/sigma_vec[k]**(alpha+1)
+        # dS[k,l] = dS[l,k]
+        # sum over k for fixed l
+        u[i,:] += np.sum(weight[:,None]*dX*F2, axis = 0)
+          
+        # inside the sum:
+        # [k,l] = weight[k]*dX[k,l]*(dS[k,l]**(alpha-2))
+        # *np.exp(-dS[k,l]**alpha/(2*sigma_vec[l]**alpha))/sigma_vec[l]**(alpha+1)
+        # = weight[k]*(x1[l]_i-x1[k]_i)*(dS[k,l]**(alpha-2))
+        # *np.exp(-dS[k,l]**alpha/(2*sigma_vec[l]**alpha))/sigma_vec[l]**(alpha+1)
+        # sum over k for fixed l        
+    u = weight*alpha*0.5*u # u[i,k] = weight[k]*u[i,k]*alpha*0.5
+ 
+    # positive derivatives of the elastic term
+    dl = np.zeros((nNeuro, nBin))
+    
+    if beta != 0:
+        
+        diffx = np.zeros((nNeuro, nBin))        
+        diffx[:, 0:-1] = np.diff(tuning, axis = 1) # x[k+1]_i - x[k]_i
+        if elastic_term_periodic:
+            diffx[:, -1] = tuning[:, 0] - tuning[:, -1]
+            
+        revdiffx =-np.roll(diffx, 1, axis = 1) # x[k-1]_i - x[k]_i
+        
+        diffS = np.sqrt(np.sum(diffx**2, axis = 0)) # sum diffx[i,k]**2 from i=1 to nNeuro
+        revdiffS = np.roll(diffS2, 1)
+        
+        if elastic_term == 'sum':
+            diffpow = diffS**(alpha-2)
+            revdiffpow = revdiffS**(alpha-2)
+            dl = diffx*diffpow + revdiffx*revdiffpow
+            dl *= 0.5*alpha
+        elif elastic_term == 'exp':
+            expdiffpow = (diffS**(alpha-2))*np.exp(-diffS**alpha/(2*Lambda**alpha))
+            exprevdiffpow = (revdiffS**(alpha-2))*np.exp(-revdiffS**alpha/(2*Lambda**alpha))
+            dl = diffx*expdiffpow + revdiffx*exprevdiffpow
+            dl *= 0.5*alpha/Lambda**3
+        elif elastic_term == 'expweight':
+            expdiffpow = (diffS**(alpha-2))*np.exp(-diffS**alpha/(2*Lambda**alpha))
+            exprevdiffpow = (revdiffS**(alpha-2))*np.exp(-revdiffS**alpha/(2*Lambda**alpha))
+            dl = weight*np.roll(weight,-1)*diffx*expdiffpow + \
+            weight*np.roll(weight,1)*revdiffx*exprevdiffpow
+            dl *= 0.5*alpha/Lambda**3
+        elif elastic_term == 'rand':
+            dl = np.random.randn((nNeuro, nBin))
+           
+        if elastic_term in ['sum', 'exp', 'expweight'] and (not elastic_term_periodic):
+            dl[:,0] = 2*dl[:,0]
+            dl[:, -1] = 2*dl[:, -1]
+
+    tuningnew = tuning + eta*u + beta*dl
+
+    tuningnew[tuningnew > upper_bound] = upper_bound
+    tuningnew[tuningnew < lower_bound] = lower_bound
+   
+    
+    return tuningnew
+
+def conditional_probability_matrix_gaussian(tuning, sigma_vec, alpha = 2):
+    nNeuro, nBin = tuning.shape
+    dS = np.zeros((nBin, nBin))
+    for i in range(tuning.shape[0]):
+        dX = tuning[i,:][None, :] - tuning[i,:][:, None] # dX[k, l]=tuning[i, l]-tuning[i, k]
+        dS += dX**2
+    dS = np.sqrt(dS)
+    D = np.exp(-dS**alpha/(2*sigma_vec[:,None]**alpha))/sigma_vec[:,None]
+    return D
+
+def evaluate_elastic_term(tuning, weight, Lambda, alpha = 2, beta = 0, elastic_term = 'sum', elastic_term_periodic = True):
+  
+    if beta == 0:
+        return 0
+    
+    nNeuro, nBin = tuning.shape
+    diffx = np.zeros((nNeuro, nBin))        
+    diffx[:, 0:-1] = np.diff(tuning, axis = 1) # x[k+1]_i - x[k]_i
+    if elastic_term_periodic:
+        diffx[:, -1] = tuning[:, 0] - tuning[:, -1]
+    diffS = np.sqrt(np.sum(diffx**2, axis = 0)) # sum diffx[i,k]**2 from i=1 to nNeuro
+
+    elastic_value = 0
+    if elastic_term == 'sum':
+        elastic_value = -0.5*np.sum(diffS**alpha)
+        
+    elif elastic_term == 'exp':
+        elastic_value = np.sum(np.exp(-diffS**alpha/(2*Lambda**alpha)))/Lambda
+        
+    elif elastic_term == 'expweight':
+        elastic_value = np.sum(weight*np.roll(weight,-1)*np.exp(-diffS**alpha/(2*Lambda**alpha)))/Lambda
+        # weight[i]*weight[i+1]
+    return elastic_value*beta
+    
+def tuning_update_inhomo_periodic(tuning, weight, kappa_mat, alpha = 2, eta = 1, beta = 0, Lambda = 1,
+                                  elastic_term = 'exp', # other options: 'expweight', 'rand'
+                                  elastic_term_periodic = True, # use closed curve
+                                 ):
+    '''Von-Mises Model, periodic b.c. in [-1, 1]^k
+    '''
+    # tuning.shape = (numNeuro, numBin)
+    # weight: vector, sum =1, length = numBin
+    # kappa_mat: matrix, kappa_mat[:,l] = [kappa_{l,1},kappa_{l,2}] # shape = (2, numBin)
+    # Lambda: number, 'kappa' parameter for the elastic term 
+    # (theoretically we can also modify it to be a vector of size 2, not implemented yet)
+    # when elastic_term takes 'rand', Lambda serves as 'kappa' parameter of noise.
+    # alpha: number, exponent of 1-cos(x_k - x_l) term in the probability distribution
+    # eta: number, coefficient of the repulsive term
+    # beta: number, coefficient of the laplacian term
+
+    nNeuro, nBin = tuning.shape
+    P = np.zeros((nBin, nBin))
+    for i in range(nNeuro):
+        dX = tuning[i,:][None, :] - tuning[i,:][:, None] # dX[k, l]=tuning[i, l]-tuning[i, k]
+        P += kappa_mat[i,:]*(1-np.cos(np.pi*dX))**(0.5*alpha)
+    P = np.exp(-P)
+    # inside exp:
+    #[k,l] = kappa_mat[0,l]*(1-np.cos(np.pi*(x1[l] - x1[k])))**(alpha/2) 
+    # + kappa_mat[1,l]*(1-np.cos(np.pi*(x2[l] - x2[k])))**(alpha/2)
+    
+    constants = np.exp(kappa_mat)/ special.iv(0, kappa_mat)/2
+    P *= np.prod(constants, axis = 0)
+    # at[k,l] position multiply by constants[0, l]*constants[1,l]
+        
+    # negative derivatives
+    u = np.zeros((nNeuro, nBin))
+    for i in range(nNeuro):
+        xi = tuning[i,:]
+        dX = xi[None, :] - xi[:, None]
+        vi = (1-np.cos(np.pi*dX))**(0.5*alpha-1)
+        u[i,:] = np.sum(weight[:,None]*vi*kappa_mat[i,:]*np.sin(np.pi*dX)*P, axis = 0)
+        # inside the sum: 
+        # [k,l] = weight[k]*(1-np.cos(np.pi*(x[l]_i-x[k]_i)))**(alpha/2-1)\
+        # *kappa_mat[i,l]*np.sin(np.pi*(x[l]_i - x[k]_i) )*P[k,l]
+        # sum over k for fixed l
+        u[i,:] += np.sum(weight[:,None]*vi*kappa_mat[i,:][:,None]*np.sin(np.pi*dX)*P.T, axis = 0)
+        # inside the sum: 
+        #  [k,l] = weight[k]*(1-np.cos(np.pi*(x[l]_i-x[k]_i)))**(alpha/2-1)\
+        # *kappa_mat[i,k]*np.sin(np.pi*(x[l]_i-x[k]_i))*P[l,k]
+        # sum over k for fixed l
+        
+    u = np.pi*0.5*alpha*weight*u # u[i,k] = constant*weight[k]*u[i,k]
+    
+      
+    # positive derivatives of the elastic term   
+    dl = np.zeros((nNeuro, nBin))
+    
+    if beta != 0:
+        diffx = np.zeros((nNeuro, nBin))        
+        diffx[:, 0:-1] = np.diff(tuning, axis = 1) # x[k+1]_i - x[k]_i
+        if elastic_term_periodic:
+            diffx[:, -1] = tuning[:, 0] - tuning[:, -1]
+        revdiffx =-np.roll(diffx, 1, axis = 1) # x[k-1]_i - x[k]_i
+        
+        diffP = np.sum(Lambda*(1-np.cos(np.pi*diffx))**(0.5*alpha), axis = 0) 
+        # diffP[k] = sum Lambda*(1-cos(pi*diffx[i,k]))**(alpha/2) from i=0 to nNeuro-1
+        diffP = np.exp(-diffP)
+        revdiffP = np.roll(diffP, 1) # np.exp(-revdiffP)
+        
+                     
+        Lambda_constant = (np.exp(Lambda)/ special.iv(0, Lambda)/2)**nNeuro #take power of nNeuro
+
+        if elastic_term == 'exp':
+            dl = (1-np.cos(np.pi*diffx))**(0.5*alpha-1)*np.sin(np.pi*diffx)*diffP
+            dl += (1-np.cos(np.pi*revdiffx))**(0.5*alpha-1)*np.sin(np.pi*revdiffx)*revdiffP
+            dl *= 0.5*alpha*np.pi*Lambda*Lambda_constant
+        elif elastic_term == 'expweight':
+            dl = weight*np.roll(weight,-1)*\
+                (1-np.cos(np.pi*diffx))**(0.5*alpha-1)*np.sin(np.pi*diffx)*diffP
+            dl += weight*np.roll(weight,1)*\
+                (1-np.cos(np.pi*revdiffx))**(0.5*alpha-1)*np.sin(np.pi*revdiffx)*revdiffP
+            dl *= 0.5*alpha*np.pi*Lambda*Lambda_constant
+        elif elastic_term == 'rand':
+            dl = np.random.vonmises(0, Lambda, size = (nNeuro, nBin))/np.pi # map to [-1, 1]
+            
+        if elastic_term in ['exp', 'expweight'] and (not elastic_term_periodic):
+            dl[:, 0] = 2*dl[:, 0]
+            dl[:, -1] = 2*dl[:, -1]
+
+    
+    # upper_bound, lower_bound = (-1, 1)
+    tuningnew = tuning + eta*u + beta*dl
+   
+    tuningnew = tuningnew - 2*np.ceil((tuningnew-1)/2.0) # map to [-1, 1]^nNeuro
+
+    return tuningnew
+
+def conditional_probability_matrix_vonmises(tuning, kappa_mat, alpha = 2):
+   
+    nNeuro, nBin = tuning.shape
+    P = np.zeros((nBin, nBin))
+    for i in range(nNeuro):
+        dX = tuning[i,:][None, :] - tuning[i,:][:, None] # dX[k, l]=tuning[i, l]-tuning[i, k]
+        P += kappa_mat[i,:]*(1-np.cos(np.pi*dX))**(0.5*alpha)     
+    P = np.exp(-P)
+        
+    constants = np.exp(kappa_mat)/ special.iv(0, kappa_mat)/2
+    P *= np.prod(constants, axis = 0) # at[k,l] position multiply by constants[0, l]*constants[1,l]*...
+    return P
+
+def evaluate_elastic_term_periodic(tuning, weight, alpha = 2, beta = 0, elastic_term = 'exp', Lambda=1,
+                                   elastic_term_periodic = True):
+    '''Evaluate the elastic term in the Von Mises Model (including beta)'''
+    
+    nNeuro, nBin = tuning.shape
+    if beta == 0:
+        return 0
+    
+    diffx = np.zeros((nNeuro, nBin))        
+    diffx[:, 0:-1] = np.diff(tuning, axis = 1) # x[k+1]_i - x[k]_i
+    if elastic_term_periodic:
+        diffx[:, -1] = tuning[:, 0] - tuning[:, -1]
+        
+    diffP = np.sum(Lambda*(1-np.cos(np.pi*diffx))**(0.5*alpha), axis = 0) 
+    # diffP[k] = sum Lambda*(1-cos(pi*diffx[i,k]))**(alpha/2) from i=0 to nNeuro-1
+    diffP = np.exp(-diffP)
+    
+    Lambda_constant = (np.exp(Lambda)/ special.iv(0, Lambda)/2)**nNeuro #take power of nNeuro
+
+    if elastic_term == 'exp':
+        elastic_value = np.sum(diffP)*Lambda_constant
+        
+    elif elastic_term == 'expweight':
+        elastic_value = np.sum(weight*np.roll(weight,-1)*diffP)*Lambda_constant
+    
+    return elastic_value*beta
+
+
+def mutual_distance(tuning):
+    nNeuro, nBin = tuning.shape
+    dS = np.zeros((nBin, nBin))
+    for i in range(nNeuro):
+        dX = tuning[i,:][None, :] - tuning[i,:][:, None] # dX[k, l]=tuning[i, l]-tuning[i, k]
+        dS += dX**2
+    dS = np.sqrt(dS)
+    return dS
